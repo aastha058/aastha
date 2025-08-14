1cfc4a8b6be0123697fd281a8cb0141fb4745f82
jagriti2004 <prakashjagriti32@gmail.com>
2025-08-14 13:44:16 +0530

    Refactor IndexController and remove old index view

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -6,5 +6,43 @@ use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
-    //
+        public function index()
+    {
+        return view('base.index');
+    }   
+    public function apoint()
+    {      
+          return view('base.appointment');
+    }
+     public function homes()
+    {
+        return view('base.homepri');
+    }
+    public function oneperson()
+    {
+        return view('base.oneperson');
+    }
+    public function limited()
+    {
+        return view('base.limited');
+    }
+    public function pink(){
+        return view('base.pink');
+    }
+    public function object()
+    {
+        return view('base.object');
+    }
+
+    public function section()
+    {
+        return view('base.section');
+    }
+    
+    Public function micro(){
+        return view('base.micro');
+    }
+     Public function nidhi(){
+        return view('base.nidhi');
+    }
 }
