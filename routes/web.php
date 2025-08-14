f6184519c47a6a2d9b00a56d90f98b3486dae9b6
Pallavi Rani <pallavicourt@gmail.com>
2025-08-14 14:04:16 +0530

    Add Reports and Secretarial Compliance pages with detailed features and benefits sections

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',[HomeController::class, 'home'])->name('home.dsc');
+Route::get('/reports', [HomeController::class, 'reports'])->name('home.reports');
+Route::get('/sc', [HomeController::class, 'sc'])->name('home.sc');
+Route::get('/bulk', [HomeController::class, 'bulk'])->name('home.bulk');
+Route::get('/account', [HomeController::class, 'account'])->name('home.account');
+Route::get('/project', [HomeController::class, 'project'])->name('home.project');
