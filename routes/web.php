357a434c1bada158bb5c0a6573a21266b92dcd03
jagriti2004 <prakashjagriti32@gmail.com>
2025-08-14 14:10:23 +0530

    Update routes to use IndexController for handling requests

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\IndexController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [IndexController::class, 'index'])->name('base.index');
+Route::get('/appointment', [IndexController::class, 'apoint'])->name('base.appointment');
+Route::get('/homepri', [IndexController::class, 'homes'])->name('base.homepri');
+Route::get('/oneperson', [IndexController::class, 'oneperson'])->name('base.oneperson');
+Route::get('/limited', [IndexController::class, 'limited'])->name('base.limited');
+Route::get('/pink', [IndexController::class, 'pink'])->name('base.pink');
+Route::get('/object', [IndexController::class, 'object'])->name('base.object');
+Route::get('/section', [IndexController::class, 'section'])->name('base.section');
+Route::get('/micro', [IndexController::class, 'micro'])->name('base.micro');
+Route::get('/nidhi', [IndexController::class, 'nidhi'])->name('base.nidhi');
+
